166a5c24b0f440db0a2b5e5d80726566fa812b00
HELZZH2B <z1741571732@gmail.com>
2024-11-03 19:20:46 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,101 @@
+criaCartao(
+    'Matemática',
+    'O que é Matemática?',
+    'A Matemática é a ciência que estuda quantidades, estruturas, mudanças e espaço. '
+)
+
+criaCartao(
+    'Português',
+    'O que é Português?',
+    'O Português é a disciplina que foca no estudo da estrutura, gramática, literatura e cultura da língua. '
+)
+
+criaCartao(
+    'História',
+    'O que é História?',
+    'A História estuda eventos, desenvolvimentos e culturas do passado humano, analisando as mudanças sociais e seus impactos. '
+)
+
+criaCartao(
+    'Química',
+    'O que é Química?',
+    'A Química é a ciência que investiga a composição, estrutura, propriedades e mudanças das substâncias. '
+)
+
+criaCartao(
+    'Biologia',
+    'O que é Biologia?',
+    'A Biologia estuda a vida e seus processos, incluindo estrutura celular, genética, ecossistemas e evolução. '
+)
+
+criaCartao(
+    'Biotecnologia',
+    'O que é Biotecnologia?',
+    'A Biotecnologia combina biologia e tecnologia para melhorar setores como agricultura, medicina e meio ambiente.'
+)
+
+criaCartao(
+    'Educação Financeira',
+    'O que é Educação Financeira?',
+    'A Educação Financeira ensina aos alunos sobre gestão pessoal, investimentos, controle orçamentário e crédito.'
+)
+
+criaCartao(
+    'Empreendedor',
+    'O que é Empreendedorismo?',
+    'O Empreendedorismo incentiva os alunos a desenvolverem pensamento inovador e habilidades para resolver problemas, aprendendo sobre planos de negócios, análise de mercado e gestão de riscos.'
+)
+
+criaCartao(
+    'Tecnologia e Robótica',
+    'O que é Tecnologia e Robótica?',
+    'A Tecnologia e Robótica envolve conhecimentos de mecânica, eletrônica e programação, onde os alunos aprendem a projetar, construir e programar robôs.'
+)
+
+criaCartao(
+    'Geografia',
+    'O que é Geografia?',
+    'A Geografia estuda a superfície da Terra e seu ambiente, incluindo relevo, clima, vegetação e atividades humanas. Analisa a relação entre o meio natural e a sociedade.'
+)
+
+criaCartao(
+    'Inglês',
+    'O que é Inglês?',
+    'O Inglês é uma língua amplamente utilizada globalmente, servindo como principal meio de comunicação em negócios, ciência e cultura.'
+)
+
+criaCartao(
+    'Sociologia',
+    'O que é Sociologia?',
+    'A Sociologia estuda o comportamento social, a estrutura social e as mudanças sociais. Analisa a interação humana, a cultura, as instituições e as questões sociais.'
+)
+
+criaCartao(
+    'Educação Física',
+    'O que é Educação Física?',
+    'A Educação Física promove a saúde física e o desenvolvimento de habilidades por meio de esportes e atividades físicas, enfatizando trabalho em equipe e espírito de competição justa.'
+)
+
+criaCartao(
+    'Projeto de Vida',
+    'O que é Projeto de Vida?',
+    'O Projeto de Vida ajuda os alunos a definir metas pessoais e direções futuras, incentivando a reflexão sobre valores, interesses e habilidades, melhorando a autoconhecimento e a tomada de decisões.'
+)
+
+criaCartao(
+    'Física',
+    'O que é Física?',
+    'A Física é a ciência que estuda a matéria, seu movimento, interações e transformação de energia. '
+)
+
+criaCartao(
+    'Redação e Leitura',
+    'O que é Redação e Leitura?',
+    'Redação e Leitura são aspectos importantes do aprendizado de línguas. A redação ensina a expressar ideias de forma eficaz e a organizar textos, enquanto a leitura melhora a compreensão e desenvolve o pensamento crítico.'
+)
+
+criaCartao(
+    'Ensino Médio',
+    'O que é o Ensino Médio?',
+    'O Ensino Médio é a etapa educacional destinada a jovens de 14 a 18 anos, com o objetivo de oferecer conhecimento aprofundado nas disciplinas e desenvolver o pensamento crítico, habilidades sociais e o crescimento pessoal. As disciplinas geralmente incluem matemática, ciências, línguas, ciências sociais e artes, e muitas escolas oferecem educação profissional e treinamento de habilidades.'
+)
